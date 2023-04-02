d82169e436bd91565f596cc3bf987883a546c886
Artem Mironchik <tawerka.power@gmail.com>
2023-04-03 00:55:34 +0300

    feat: change search bar into a function component

--- react-components-app/src/components/searchbar/SearchBar.tsx
+++ react-components-app/src/components/searchbar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { FC, useCallback } from 'react';
 import searchIcon from '../../images/search.svg';
 
 type SearchBarProps = {
@@ -6,31 +6,28 @@ type SearchBarProps = {
   value: string;
 };
 
-class SearchBar extends Component<SearchBarProps, never> {
-  handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { handleSearchValue } = this.props;
-    handleSearchValue(e.target.value);
-  };
+const SearchBar: FC<SearchBarProps> = ({ handleSearchValue, value }) => {
+  const handleSearch = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => handleSearchValue(e.target.value),
+    [handleSearchValue]
+  );
 
-  render() {
-    const { value } = this.props;
-    return (
-      <div className="flex w-full relative border border-solid border-black">
-        <img
-          src={searchIcon}
-          alt="Search icon"
-          className="absolute top-1/2 left-2 max-w-[16px] translate-y-[-50%]"
-        />
-        <input
-          name="Search"
-          placeholder="Поиск"
-          value={value}
-          className="w-full pl-8"
-          onChange={this.handleSearch}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="flex w-full relative border border-solid border-black">
+      <img
+        src={searchIcon}
+        alt="Search icon"
+        className="absolute top-1/2 left-2 max-w-[16px] translate-y-[-50%]"
+      />
+      <input
+        name="Search"
+        placeholder="Поиск"
+        value={value}
+        className="w-full pl-8"
+        onChange={handleSearch}
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
